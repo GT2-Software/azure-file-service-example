2a127e6541b6cbc4dc99d579f5ca97f4a27ba41c
Giovani Meza <giovani.meza@gt2software.dev>
2024-12-27 22:39:59 -0600

    Adds crontab

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
+#. $HOME/.bashrc
 set -e
 echo "Entrypoint script is running"
+chmod 0644 /etc/periodic/cron-log-files
+mkdir -p ${LOG_ROOT_PATH}
+touch ${LOG_ROOT_PATH}/cron.log
+chmod 777 ${LOG_ROOT_PATH}/cron.log
+crontab /etc/periodic/cron-log-files
+crond -f /etc/periodic/cron-log-files &
+#cron
+#rc-service crond start && rc-update add crond
+
 #/usr/sbin/sshd
 #adduser sop_prod -D
 #java -XshowSettings:properties -version
 #exec java -Djava.security.egd=file:///dev/urandom -XX:MaxRAMPercentage=${MEM_MAX_RAM_PERCENTAGE} -Xss${MEM_XSS} -XX:+UseContainerSupport ${JVM_ARGS} -Xmx12g -XX:ParallelGCThreads=4 -XX:ConcGCThreads=2 -Djava.util.concurrent.ForkJoinPool.common.parallelism=4 -jar payara-micro.jar "$@"
-#exec java -Dkubernetes.log4j.useProperties="true" -jar payara-micro.jar "$@" "--deploymentDir" "/opt/payara/deployments" "--port" "8080" "--systemproperties" "log4j.properties"
-sleep infinity
+exec java -Dkubernetes.log4j.useProperties="true" -jar payara-micro.jar "$@" "--deploymentDir" "/opt/payara/deployments" "--port" "8080"
+#sleep infinity
