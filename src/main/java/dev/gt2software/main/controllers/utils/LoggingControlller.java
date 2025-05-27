69f4dbc5cb1b6c7dbb484aa381a70a7f69ef7ac0
Giovani David Meza Poggio <giovani.meza@gt2software.dev>
2025-05-26 21:07:23 -0600

    Adds echo service

---
+++ src/main/java/dev/gt2software/main/controllers/utils/LoggingControlller.java
@@ -0,0 +1,41 @@
+package dev.gt2software.main.controllers;
+
+import java.util.Optional;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import dev.gt2software.main.Constants;
+import dev.gt2software.main.models.ResponseModel;
+import dev.gt2software.main.services.v1.PrintService;
+
+@Path("version")
+public class LoggingControlller {
+    private final ResponseModel responseModel = ResponseModel.getInstance();
+    private PrintService printService = new PrintService();
+    private Logger logger = LogManager.getLogger(LoggingControlller.class);
+
+    public LoggingControlller() {
+    }
+
+    private void setSystemProperties() {
+
+    }
+
+    @GET
+    // @Path("echo")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.WILDCARD)
+    public Response logToEchoClient() {
+        responseModel.setResponse(printService.echoClient());
+        return responseModel.getResponse();
+    }
+}
